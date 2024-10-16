76051f5e6abdc8adf2cd4351b4ed49ab5c314f84
affctionP <affctionhossiny@yahoo.com>
2024-10-16 09:25:08 +0330

    cahnge bash

--- scripts/preprocess_commonvoice_fa/preprocess_commonvoice_fa_df.sh
+++ scripts/preprocess_commonvoice_fa/preprocess_commonvoice_fa_df.sh
@@ -5,11 +5,19 @@ full_path=$0
 CONFIG_PATH=$(dirname "$full_path")
 echo $CONFIG_PATH
 
+# Assign arguments passed when running the script
+DATASET_PATH=$1
+META_PATH=$2
 
-DATASET_PATH="PATH TO DATASET"
+# Fixed variables
 OUTPUT_PATH="/output_data_atefeh"
 NUM_WORKERS=4
-META_PATH='PATH OF META CSV'
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <DATASET_PATH> <META_PATH>"
+    exit 1
+fi
 
 python -m tac2persian.data_preprocessing.preprocess_commonvoice_fa_df --dataset_path="$DATASET_PATH" \
                                                                    --output_path="$OUTPUT_PATH" \
