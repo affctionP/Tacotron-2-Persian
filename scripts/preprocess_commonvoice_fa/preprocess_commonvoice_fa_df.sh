b85926413c82b8a5e1e9ecb3aa6033faf5acd977
affctionP <affctionhossiny@yahoo.com>
2024-10-16 08:44:03 +0330

    add bash script

---
+++ scripts/preprocess_commonvoice_fa/preprocess_commonvoice_fa_df.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Get full path to the config file automatically
+full_path=$0
+CONFIG_PATH=$(dirname "$full_path")
+echo $CONFIG_PATH
+
+
+DATASET_PATH="PATH TO DATASET"
+OUTPUT_PATH="/output_data_atefeh"
+NUM_WORKERS=4
+
+python -m tac2persian.data_preprocessing.preprocess_commonvoice_fa_df --dataset_path="$DATASET_PATH" \
+                                                                   --output_path="$OUTPUT_PATH" \
+                                                                   --config_path="$CONFIG_PATH" \
+                                                                   --num_workers="$NUM_WORKERS"
